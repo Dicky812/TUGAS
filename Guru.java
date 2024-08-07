5225bac254090b7d4a234764df81ecb193965202
Mochamad Dicky <m_ardiansyah_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:55:41 +0700

    Add files via upload

---
+++ Guru.java
@@ -0,0 +1,13 @@
+public class Guru {
+    int id;
+    String nama, mapel, alamat;
+
+    /*Construktor nama dengan class */
+    public Guru(){
+        id = 0;
+        nama = "";
+        mapel = "";
+        alamat = "";
+
+    }
+}
---
+++ tugasRpl.java
@@ -0,0 +1,66 @@
+public class tugasRpl {
+    public static void main(String[] args) {
+        //make object
+        Guru Buaulia = new Guru();
+        Guru Bufeni = new Guru();
+        Guru Bupasha  = new Guru();
+        Guru Pakdiaur = new Guru();
+        Guru Paknasikin = new Guru();
+
+        //Bu aulia
+        Buaulia.id = 1;
+        Buaulia.nama = "Bu aulia";
+        Buaulia.mapel ="Guru reproduktif RPL";
+        Buaulia.alamat ="Malang";
+        System.out.println(Buaulia.id);
+        System.out.println(Buaulia.nama);
+        System.out.println(Buaulia.mapel);
+        System.out.println(Buaulia.alamat);
+       
+
+
+        //Bu feni
+        Bufeni.id = 2;
+        Bufeni.nama ="Bu feni";
+        Bufeni.mapel ="Guru matematika";
+        Bufeni.alamat ="Malang";
+        System.out.println(Bufeni.id);
+        System.out.println(Bufeni.nama);
+        System.out.println(Bufeni.mapel);
+        System.out.println(Bufeni.alamat);
+
+        //Bu pasha
+        Bupasha.id = 3;
+        Bupasha.nama ="Bu pasha";
+        Bupasha.mapel ="Guru reproduktif RPL";
+        Bupasha.alamat ="Malang";
+        System.out.println(Bupasha.id);
+        System.out.println(Bupasha.nama);
+        System.out.println(Bupasha.mapel);
+        System.out.println(Bupasha.alamat);
+        
+        //Pak diaur
+        Pakdiaur.id = 4;
+        Pakdiaur.nama ="Pak diaur";
+        Pakdiaur.mapel ="Guru pjok";
+        Pakdiaur.alamat ="Malang";
+        System.out.println(Pakdiaur.id);
+        System.out.println(Pakdiaur.nama);
+        System.out.println(Pakdiaur.mapel);
+        System.out.println(Pakdiaur.alamat);
+
+        //Pak nasikin
+        Paknasikin.id = 5;
+        Paknasikin.nama ="Pak naasikin";
+        Paknasikin.mapel ="Guru pendidikan agama islam";
+        Paknasikin.alamat ="Malang";
+        System.out.println(Paknasikin.id);
+        System.out.println(Paknasikin.nama);
+        System.out.println(Paknasikin.mapel);
+        System.out.println(Paknasikin.alamat);
+
+        
+    }
+    
+}
+
